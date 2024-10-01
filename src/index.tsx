f99717745ff4c9d8196aaff53a8aad65252d6145
Aleh Baradzenka <baradzenka@gmail.com>
2024-10-02 02:14:48 +0300

    Added login/pass and inn info

--- src/index.tsx
+++ src/index.tsx
@@ -25,6 +25,20 @@ root.render(
 		</React.StrictMode>
 	);
 
+/*
+  Логин     Пароль
+sf_student1	4i2385j
+sf_student3	6z9ZFRs
+sf_student2	lV8xjCH
+sf_student4	Br1+tbG
+sf_student5	LuwAwJf
+sf_student6	eczpWCB
+sf_student7	P6VcKNf
+sf_student8	5QB0KM/
+sf_student9	DTdEwAn
+
+INN: 7710137066
+*/
 
 export function SiteContent(): JSX.Element
 {
@@ -40,7 +54,7 @@ export function SiteContent(): JSX.Element
 		, []);
 
 	return (
-			<RouterDOM.BrowserRouter>
+			<RouterDOM.HashRouter>
 				<RouterDOM.Routes>
 					<RouterDOM.Route element={<Layout/>}>
 						<RouterDOM.Route index element={<MainStart/>}/>
@@ -50,7 +64,7 @@ export function SiteContent(): JSX.Element
 					</RouterDOM.Route>
 					<RouterDOM.Route path="*" element={<PageNotFound/>}/>
 				</RouterDOM.Routes>
-			</RouterDOM.BrowserRouter>
+			</RouterDOM.HashRouter>
 		);
 }
 
